69cad2f483c8d012996b219989f409df308a1451
jgalvao <jfelipe16@gmail.com>
2023-08-11 14:30:09 -0300

    created home page listing all filmes on the cinema

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
 function Home() {
+   const [filmes, setFilmes] = useState([]);
+   const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+      async function loadFilmes() {
+         const response = await api.get('movie/now_playing', {
+            params: {
+               api_key: 'ac4d2bdca1869bbe0db21861340c3623',
+               language: 'pt-BR',
+               page: 1,
+            },
+         });
+
+         setFilmes(response.data.results.slice(0, 10));
+         setLoading(false);
+      }
+
+      loadFilmes();
+   }, []);
+
+   if (loading) {
+      return (
+         <div className="loading">
+            <h2>Carregando filmes ...</h2>
+         </div>
+      );
+   }
+
    return (
-      <div>
-         <h1>Bem vindo a Home</h1>
+      <div className="container">
+         <div className="lista-filmes">
+            {filmes.map((filme) => {
+               return (
+                  <article key={filme.id}>
+                     <strong>{filme.title}</strong>
+                     <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                     <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                  </article>
+               );
+            })}
+         </div>
       </div>
    );
 }
